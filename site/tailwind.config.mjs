222ee7aa92f90fafae1b4ccc4d9a26254699be60
Eli Lennox <eli.lennox@sentry.io>
2024-07-23 17:56:30 -0400

    color presets in tailwind

--- site/tailwind.config.mjs
+++ site/tailwind.config.mjs
@@ -2,6 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      background: '#F2F0ED',
+      black: '#0D1B2A',
+      teal: '#0A9396',
+      orange: '#FF763E',
+      lightGray: '#A5A5A5',
+      midnight: '#1E1E1E',
+      taupe: '#E8E3DC',
+      rust: '#BB3E04',
+      purple: '#6E5494'
+    },
 		extend: {},
 	},
 	plugins: [],

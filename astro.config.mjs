10ad4055aa19671ee117ec021c163f714af97fa8
Eli Lennox <eli.lennox@sentry.io>
2024-07-30 12:07:33 -0400

    merge into main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    tailwind(),
+    react({
+      include: ['**/react/**'],
+      experimentalReactChildren: true
+    })
+  ]
 });

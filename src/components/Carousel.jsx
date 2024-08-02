f9ea8d9595b5a62cdae7922d4ed67cfd099f1031
Dylan Coots <dc@buildmidwestern.com>
2024-08-02 16:58:24 -0500

    remove @responsive (its deprecated)

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -91,7 +91,7 @@ const Carousel = ({companies}) => {
           </a>
         </div>
       </div>
-      <div className="relative container-spill xl:pl-[3rem] carousel-wrapper lg:carousel-wrapper-right">
+      <div className="relative container-spill xl:pl-[3rem] carousel-wrapper carousel-wrapper-right">
         <div
           ref={carousel}
           className="carousel-container no-scrollbar overflow-auto touch-pan-x relative flex gap-[1rem] scroll-smooth snap-x snap-mandatory z-0"
